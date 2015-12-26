3c16b6af50918cd1e40d5b3248f7d9dbcb53ded1
MODICT <MODICT@users.noreply.github.com>
2015-12-26 04:24:17 +0100

    updated to new version 1.0.4.2
    The version 1.0.4.2 comes with helper functions to render shapes. Check
    out http://i-pv.org/blob2.html for tutorials.

--- blob1.0.4.2.js
+++ blob1.0.4.2.js
@@ -1,6 +1,94 @@
 var coordinates = [0,0];
 		var areaBlob = {};
 		var blobJsCanvasCounter = 0;
+		var blobTools ={
+			vBelt:function vBelt(r,R,d,i,nodeCount) {
+				var sum = r+R+d;
+				var theta = Math.asin((R-r)/(R+r+d));
+				var A = Math.sqrt(Math.pow(R+r+d,2)-Math.pow(R-r,2));
+				var beta = Math.atan(A/r);
+				var y = Math.abs(Math.PI-i/nodeCount*Math.PI*2);
+				var x = Math.abs(Math.asin(sum*Math.sin(y)/R)) || 0;
+				var z = Math.PI-x-y;
+				var U = Math.sqrt(Math.pow(sum,2)+Math.pow(R,2)-2*sum*R*Math.cos(z));
+				var delta = (Math.PI-2*theta)/2/Math.PI/2;
+				var betaCurrent = i/nodeCount <= 0.5 ? Math.PI*2*(i/nodeCount-delta) :  Math.PI*2*(1-i/nodeCount-delta);
+				
+				return blobTools.retrieveInterval(-0.5,delta,i,nodeCount)*r+blobTools.retrieveInterval(delta,delta+beta/Math.PI/2,i,nodeCount)*r/Math.cos(betaCurrent)+blobTools.retrieveInterval(delta+beta/Math.PI/2,1-delta-beta/Math.PI/2,i,nodeCount)*U+blobTools.retrieveInterval(1-delta-beta/Math.PI/2,(3/2*Math.PI+theta)/Math.PI/2,i,nodeCount)*r/Math.cos(betaCurrent)+blobTools.retrieveInterval((3/2*Math.PI+theta)/Math.PI/2,1,i,nodeCount)*r;
+			},
+			
+			retrieveInterval:function retrieveInterval (a,b,i,nodeCount){
+					var mid = (a+b)/2;
+					var distance = Math.abs(a-mid);
+					return Math.round(0.5+distance-Math.abs(mid-i/nodeCount));
+			},
+			
+			wiggle:function wiggle(R,Rx,teethCount,i,nodeCount){
+			var teethWidth = nodeCount/teethCount;
+			var teethSegment = Math.floor((i%teethWidth)/(teethWidth/3)); 
+			return (Math.floor(i/teethWidth)%2)*(Math.abs(Math.sign(teethSegment*(teethSegment-2)))*(Rx-R)/R+Math.abs(Math.sign((teethSegment-1)*(teethSegment-2)))*((Rx-R)/R*(i%(teethWidth/3))/teethWidth*3)+Math.abs(Math.sign(teethSegment*(teethSegment-1)))*((Rx-R)/R-(Rx-R)/R*(i%(teethWidth/3))/teethWidth*3))
+			},
+			
+			ellipse:function ellipse (a,i,nodeCount,b){
+				b = b === undefined ? 1 : b;
+				var tanTheta = Math.abs(Math.tan(i/nodeCount*Math.PI*2));
+				var x = Math.sqrt(Math.pow(a,2)*Math.pow(tanTheta,2)*Math.pow(b,2)/(Math.pow(a,2)+Math.pow(tanTheta,2)*Math.pow(b,2)));
+				var y = tanTheta === 0 ? b : x/tanTheta;
+				/*x^2/a^2+(x/tanTheta)^2=1
+				x^2*tanTheta^2+x^2*a^2 = a^2*tanTheta^2;
+				x^2(tanTheta^2+a^2) = a^2*tanTheta^2;*/
+				return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+			},
+			
+			droplet:function droplet (roundness,i,nodeCount) {
+				var coefficient = 1/(1-roundness) === Infinity ? 0 : 1/(1-roundness);
+				return roundness+(coefficient*Math.pow((i-nodeCount*(1-roundness))/nodeCount,2))*blobTools.retrieveInterval(-0.5,1-roundness,i,nodeCount)+blobTools.retrieveInterval(roundness,1,i,nodeCount)*(coefficient*Math.pow((i-nodeCount*roundness)/nodeCount,2));
+			},
+			star:function star (vertexLength,vertexCount,sharpness,i,nodeCount) {
+				return 1-vertexLength+vertexLength*Math.abs(Math.pow(Math.sin(vertexCount*Math.PI*i/nodeCount-Math.PI/2),10*sharpness))
+			},
+			heart:function heart(a,b,i,nodeCount) {
+				var r = 1/(1+Math.pow(b,-5));
+				return blobTools.retrieveInterval(-0.5,0.25,i,nodeCount)*1/(1+Math.pow(b,-1*(i/nodeCount*40-5)))+blobTools.retrieveInterval(0.75,1,i,nodeCount)*1/(1+Math.pow(b,-1*(10-(i/nodeCount-0.75)*40-5)))+blobTools.retrieveInterval(0.25,0.75,i,nodeCount)*blobTools.ellipse(a,i+nodeCount/4,nodeCount,r);
+			},
+			polygonAngular:function polygonAngular(a,i,nodeCount){
+				var length = a.length;
+				var residualAngle = Math.PI*2;
+				for (var j=0;j<a.length;j+=2){
+					residualAngle -= a[j]/360*Math.PI*2;
+				}
+				var distanceRlastR1=Math.sqrt(Math.pow(a[a.length-1],2)+Math.pow(a[1],2)-2*a[a.length-1]*a[1]*Math.cos(residualAngle+a[0]/360*Math.PI*2));
+				var zeta = Math.asin(a[a.length-1]*Math.sin(residualAngle+a[0]/360*Math.PI*2)/distanceRlastR1);
+				var r0 = residualAngle!==0?Math.sin(zeta)*a[1]/Math.sin(Math.PI-zeta-a[0]/360*Math.PI*2):a[a.length-1];
+				var angle = i/nodeCount*360;
+				for (var j = 0,sum =0;(i/nodeCount*360>a[j]+sum && j<a.length);j+=2){
+					sum += a[j];
+					angle = i/nodeCount*360-sum;
+				}
+				j += 1;
+				angle = angle/360*Math.PI*2;
+				a[-1] = r0;
+				a[length] = residualAngle/Math.PI/2*360;
+				a[length+1] = r0;
+				var distanceRlastR0 = Math.sqrt(Math.pow(a[length-1],2)+Math.pow(r0,2)-2*a[length-1]*r0*Math.cos(residualAngle));
+				var distanceRjRjmin2 = Math.sqrt(Math.pow(a[j],2)+Math.pow(a[j-2],2)-2*a[j]*a[j-2]*Math.cos(a[j-1]/360*Math.PI*2));
+				var r;
+				if (j>length) {
+					if (Math.pow(r0,2)>= Math.pow(a[j-2],2)+Math.pow(distanceRlastR0,2)) {
+						r = Math.abs(a[j-2]*r0*Math.sin(residualAngle)/distanceRlastR0/Math.sin(-angle+Math.asin(r0*Math.sin(residualAngle)/distanceRlastR0)));
+					} else {
+						r = Math.abs(a[j-2]*r0*Math.sin(residualAngle)/distanceRlastR0/Math.sin(Math.PI-angle-Math.asin(r0*Math.sin(residualAngle)/distanceRlastR0)));
+					}
+				} else {
+					if (Math.pow(a[j],2)>=Math.pow(a[j-2],2)+Math.pow(distanceRjRjmin2,2)) {
+						r = Math.abs(a[j-2]*a[j]*Math.sin(a[j-1]/360*Math.PI*2)/distanceRjRjmin2/Math.sin(-angle+Math.asin(a[j]/distanceRjRjmin2*Math.sin(a[j-1]/360*Math.PI*2))));
+					} else {
+						r = Math.abs(a[j-2]*a[j]*Math.sin(a[j-1]/360*Math.PI*2)/distanceRjRjmin2/Math.sin(Math.PI-angle-Math.asin(a[j]/distanceRjRjmin2*Math.sin(a[j-1]/360*Math.PI*2))));
+					}
+				}
+				return r;
+			}
+		}
 		
 		function initiateBlobArea (areaObject) {
 		//{id:"drawHere",width:1000,height:1000,top:0,left:0}
@@ -80,8 +168,12 @@ var coordinates = [0,0];
 			var attraction = blobObject.attraction || 0.5;
 			var bounceOn = blobObject.bounce || 1;
 			var xray = blobObject.xray || 0;
+			var parentTop = blobObject.top === undefined ? "0px" : blobObject.top+"px";
+			var parentLeft = blobObject.left === undefined ? "0px" : blobObject.left+"px";
 			var parameter = blobObject.parameter || function(){return 1;};
 			var parameterTime = blobObject.parameterTime || function(){return 1;};
+			var offset = blobObject.offset===undefined? [0,0] : blobObject.offset;
+			var isStatic = blobObject.isStatic ? blobObject.isStatic : 0;
 			//globals
 			
 			//append the canvas element
@@ -91,19 +183,24 @@ var coordinates = [0,0];
 			createdElement.setAttribute("height",height);
 			if (blobObject.parent) {
 				document.getElementById(blobObject.parent).appendChild(createdElement);
+				document.getElementById(ID).style.position = "absolute";
+				document.getElementById(ID).style.top = parentTop;
+				document.getElementById(ID).style.left = parentLeft;
 			} else {
 				document.body.appendChild(createdElement);
+				document.getElementById(ID).style.position = "absolute";
+				document.getElementById(ID).style.top = areaBlob.top;
+				document.getElementById(ID).style.left = areaBlob.left;
 			}
-			document.getElementById(ID).style.position = "absolute";
+			
 			document.getElementById(ID).style.zIndex = areaBlob.zIndex - 1;
-			document.getElementById(ID).style.top = areaBlob.top;
-			document.getElementById(ID).style.left = areaBlob.left;
+			
 			var canvas = document.getElementById(ID);
 			var context = canvas.getContext("2d");
 			//append the canvas element
 			
 			function animate(timestamp) {
-				var thetaMouse = Math.atan((origin[1]-coordinates[1])/(origin[0]-coordinates[0]));
+				var thetaMouse = Math.atan((origin[1]+offset[1]-coordinates[1])/(origin[0]+offset[0]-coordinates[0]));
 				context.clearRect(0,0,width,height);
 				context.globalAlpha = xray;
 				var bounceCoeff = 1;
@@ -126,11 +223,11 @@ var coordinates = [0,0];
 				var currentTime = (timestamp-startTime)/1000;
 				for (i=0;i<nodeCount;i++) {
 					var theta = thetas[i];
-					var sign = Math.sign(origin[0]-coordinates[0])*Math.sign(origin[0]-circlePositions[i][0]);
+					var sign = Math.sign(origin[0]+offset[0]-coordinates[0])*Math.sign(origin[0]-circlePositions[i][0]);
 					if (i === 0 || i === nodeCount/2) {
-						sign = -Math.sign(origin[0]-coordinates[0]);
+						sign = -Math.sign(origin[0]+offset[0]-coordinates[0]);
 					}
-					var distance = Math.min(Math.sqrt(Math.pow(coordinates[0]-origin[0],2)+Math.pow(coordinates[1]-origin[1],2)),blobRadius)/blobRadius;
+					var distance = Math.min(Math.sqrt(Math.pow(coordinates[0]-origin[0]-offset[0],2)+Math.pow(coordinates[1]-origin[1]-offset[1],2)),blobRadius)/blobRadius;
 					var thetaDiff = thetaMouse-theta;
 					circlePositions[i]=[origin[0]+parameterTime(i,nodeCount,currentTime)*(blobRadii[i]+attraction*blobRadii[i]*bounceCoeff*sign*distance*Math.cos(thetaDiff))*Math.sin(i*Math.PI*2/nodeCount),origin[1]-parameterTime(i,nodeCount,currentTime)*(blobRadii[i]+attraction*blobRadii[i]*bounceCoeff*sign*distance*Math.cos(thetaDiff))*Math.cos(i*Math.PI*2/nodeCount)];
 					orCirclePositions[i]=[origin[0]+parameterTime(i,nodeCount,currentTime)*(blobRadii[i]+attraction*blobRadii[i]*bounceCoeff*sign*distance*Math.cos(thetaDiff))*Math.sin(i*Math.PI*2/nodeCount),origin[1]-parameterTime(i,nodeCount,currentTime)*(blobRadii[i]+attraction*blobRadii[i]*bounceCoeff*sign*distance*Math.cos(thetaDiff))*Math.cos(i*Math.PI*2/nodeCount)];
@@ -198,7 +295,6 @@ var coordinates = [0,0];
 				context.stroke();
 				context.globalAlpha = fillOpacity;
 				context.fill();
-				window.requestAnimationFrame(animate);
 				
 				if (bounce !== 0 && (Math.round(bounceStrength*1000)/1000 === 0)) {
 					bounce = bounce-1;
@@ -207,12 +303,14 @@ var coordinates = [0,0];
 					bounceStrength = Math.PI*4;
 				}
 			
-				var distance = Math.sqrt(Math.pow(coordinates[0]-origin[0],2)+Math.pow(coordinates[1]-origin[1],2));
+				var distance = Math.sqrt(Math.pow(coordinates[0]-origin[0]-offset[0],2)+Math.pow(coordinates[1]-origin[1]-offset[1],2));
 				var thresholdRadius = (Math.max.apply(null,blobRadii)+Math.min.apply(null,blobRadii))/2;
 				if (Math.round((thresholdRadius+attraction*thresholdRadius+lineWidth/2)/distance*10)/10 === 1 && bounceOn === 1) {
 					bounce = 1;
 					bounceStrength = Math.PI*4;
 				}
+				
+				((function (){var x = {0:function(){window.requestAnimationFrame(animate)},1:function(){}}; return x[isStatic]})())();
 			}
 			window.requestAnimationFrame(animate);
 		};
